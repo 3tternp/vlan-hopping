5f2fcb1c959662a22ef076c27b23a26a1b75ecd8
astra <punkx.0gravity@gmail.com>
2023-07-11 13:55:52 +0545

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+#!/usr/bash
+#requirement installation for frogger2
+
+sudo apt install yersinia -y 
+sudo apt install ettercap-graphical -y 
+sudo apt install screen -y 
+sudo apt install arp-scan -y 
+sudo apt-get install vlan -y 
+sudo apt install ethtool -y 
+sudo apt install tshark -y 
+
